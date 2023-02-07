e6c2722272a29c4d8f31486805f145a1621527c6
crazy-PC <jiangcq333@foxmail.com>
2023-02-07 09:24:17 +0800

    PC submit

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -1,11 +1,50 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import {createRouter, createWebHistory} from 'vue-router'
+import PKIndexView from "../../views/pk/PKIndexView.vue";
+import RankListIndex from "../../views/ranklist/RankListIndex.vue";
+import RecordIndex from "../../views/record/RecordIndex.vue";
+import UserBotIndex from "../../views/user/bot/UserBotIndex.vue";
+import NotFound from "../../views/error/NotFound.vue";
 
-
-const routes = []
+const routes = [
+    {
+        path: "/",
+        name: "home",
+        redirect: "/pk/",
+    },
+    {
+        path: "/pk/",
+        name: "pk_index",
+        component: PKIndexView,
+    },
+    {
+        path: "/ranklist/",
+        name: "ranklist_index",
+        component: RankListIndex,
+    },
+    {
+        path: "/record/",
+        name: "record_index",
+        component: RecordIndex,
+    },
+    {
+        path: "/user/bot/",
+        name: "user_bot_index",
+        component: UserBotIndex,
+    },
+    {
+        path: "/404/",
+        name: "404",
+        component: NotFound,
+    },
+    {
+        path: "/:catchAll(.*)",
+        redirect: "/404/",
+    },
+]
 
 const router = createRouter({
-  history: createWebHistory(),
-  routes
+    history: createWebHistory(),
+    routes
 })
 
 export default router
